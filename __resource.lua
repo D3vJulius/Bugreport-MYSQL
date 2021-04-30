e76e43919ad0ba46c3ffdbb3871557c227f883d3
Julian <julianvictorfjeld@gmail.com>
2021-04-30 16:06:57 +0200

    Bugreport-MySQL
    Bugreport with MySQL included. Website updates coming!

--- __resource.lua
+++ __resource.lua
@@ -3,6 +3,6 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 client_script "client.lua"
 
 server_scripts {
-    "server.lua",
+    "server/server.lua",
     "@mysql-async/lib/MySQL.lua"
 }
