a5150b860637c85e32f4181ff891faf819b7970e
Julian <julianvictorfjeld@gmail.com>
2021-05-01 13:46:45 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,13 +1,23 @@
+  -- MYSQL Security
+
+    function escape_sqli(source)
+        local replacements = { ['"'] = '\\"', ["'"] = "\\'" }
+        return source:gsub( "['\"]", replacements )
+    end
+
     -- Command register
-RegisterCommand("bugreport", function(source, args)
+
+    RegisterCommand("bugreport", function(source, args)
     local argString = table.concat(args, " ")
+
     -- MySQL Insert to table
+
     MySQL.Async.fetchAll("INSERT INTO bugs (id, name, bug) VALUES(@source, @name, @bug)",     
-    {["@source"] = GetPlayerIdentifiers(source)[1], ["@name"] = GetPlayerName(source), ["@bug"] = argString},
-
+    {["@source"] = GetPlayerIdentifiers(source)[1], ["@name"] = GetPlayerName(source), ["@bug"] = escape_sqli(argString)},
+    
     -- ReturnMessage
-    function (result)
-        TriggerClientEvent("returnmessage", source, "".. argString.. "^0")
 
+    function (result)
+        TriggerClientEvent("chatMessage", source, "[BUG REPORT]", {204, 0, 0}, argString)
     end)
 end)
